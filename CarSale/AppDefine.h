998a2d1786a25dd85375ab3024c43fc000774942
huangluyang <Huangluyang@HuangdeiMac.local>
2015-03-26 09:15:48 +0800

    appdefine
    add app define

---
+++ CarSale/AppDefine.h
@@ -0,0 +1,17 @@
+//
+//  AppDefine.h
+//  CarSale
+//
+//  Created by HuangLuyang on 15-3-25.
+//  Copyright (c) 2015年 Huang Luyang. All rights reserved.
+//
+
+#ifndef CarSale_AppDefine_h
+#define CarSale_AppDefine_h
+
+static NSString*const BaseURLString = @"http://10.12.30.93:6655/webservice/";
+
+#define MenuTable @"GetMenuTable"
+#define CarTable @"GetCarTable"
+
+#endif
